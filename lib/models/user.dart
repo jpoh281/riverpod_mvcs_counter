75ff2f4528d532f7545e3bdc0fb12352eeb4c546
jpoh281 <hjp4869@naver.com>
2022-03-21 00:01:37 +0900

    ♻️ Rename User, String -> AsyncValue<String>

---
+++ lib/models/user.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final user = StateNotifierProvider<User, AsyncValue<String>>((ref) => User());
+
+class User extends StateNotifier<AsyncValue<String>>{
+  User():super(AsyncData(""));
+
+  setCurrentUser(String currentUser){
+    state = AsyncData(currentUser);
+  }
+}
